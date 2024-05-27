89fe41dff3dd2fbf0d8116f863467a3a3b5b5443
Stan <67479737+stanyvh@users.noreply.github.com>
2024-05-27 19:01:54 +0200

    done

--- app.py
+++ app.py
@@ -1,92 +1,40 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
 import pandas as pd
 import numpy as np
-import time
+import joblib
+import sklearn
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+st.title("Predicting Life Satisfaction based on GDP per Capita.")
+st.write("This Machine Learning model will make a prediction for the Life Satisfaction (1-10) of a country based on it's GDP per Capita.")
+st.write("Two values for you to test: ")
+st.write("Belgium's GDP per Capita: 53659.317, original value: 6.8")
+st.write("Colombia's GDP per Capita: 6971.669, original value: 5.7")
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+# Load the model
+model = joblib.load('model.pkl')
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+# Ask input value 
+st.text_input("GDP per Capita value: ", key="value")
 
-st.line_chart(chart_data)
+if st.session_state.value:
+    # converting input to float
+    prediction_value = float(st.session_state.value)
+    # storing in 2D array
+    data = np.array([[prediction_value]])
+    # Make predictions
+    predictions = model.predict(data)
+    # Print the predictions
+    print(predictions) # terminal
+    st.write("Life Satisfaction Prediction: ", predictions)
 
-map_data = pd.DataFrame({
-  'lat': [50.876410],
-  'lon': [4.714970]
-})
+if st.checkbox('Show data'):
+    training_data = pd.read_csv("training_data.csv", index_col=0)
+    training_data
 
-st.map(map_data)
-
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-st.text_input("Your name", key="name")
-
-# You can access the value at any point with:
-st.session_state.name
-
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    chart_data
-
-df = pd.DataFrame({
-'first column': [1, 2, 3, 4],
-'second column': [10, 20, 30, 40]
-})
-
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
-
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
-
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
-
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-left_column.button('Press me!')
-
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
-
-
-'Starting a long computation...'
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
+if st.checkbox('Show me valuable Life Satisfaction advice'):
+    st.write("Best Pain au Chocolate I every had.")
+    map_data = pd.DataFrame({
+        'lat': [50.880660],
+        'lon': [4.695650]
+    })
+    st.map(map_data)
